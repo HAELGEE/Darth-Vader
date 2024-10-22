dc4144a4753ef1fd0e3b6c22b1e327a154f96f75
HAELGEE <helge1337@live.com>
2024-10-22 14:05:36 +0200

    Update

--- Darth Vader/Program.cs
+++ Darth Vader/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace Darth_Vader;
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace Darth_Vader;
 
 internal class Program
 {
@@ -6,4 +10,20 @@ internal class Program
     {
         Console.WriteLine("Hej jag heter Måns");
     }
+
+    // https://github.com/HAELGEE/Darth-Vader
+
+//  Git
+//  ===
+//  1. Låt alla gruppmedlemmar(som inte redan har ett) skapa ett konto på GitHub
+//  2. Låt en av gruppmedlemmarna skapa ett nytt(C# Console) projekt i Visual Studio
+//  3. Lägg upp koden i ett (privat eller publikt) repo på GitHub genom Git->Create Git Repository...
+//  4. Lägg till samtliga deltagare som kollaboratörer på repot i GitHub
+//  5. Låt de andra gruppmedlemmarna klona repot till sina maskiner
+//  6. Laborera nu fritt med Commit, Fetch, Pull, Push och Synk
+//  7. Låta en gruppmedlem skapa en ny branch
+//  8. Lägg till minst en commit till den nya branchen
+//  9. Hoppa mellan brancharna och notera att de förändrade filerna uppdateras
+//  10. Mergea nu in den nya branchen in i den första(så de commits som gjorts på den nya branachen nu dyker upp även i den första)
+
 }
