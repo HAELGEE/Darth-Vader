6ca1790eb1b6118bf0b0d41f79e4f3854c4eac53
EveLexie <Evelina.hubinette@hotmail.se>
2024-10-22 14:40:13 +0200

    tet3

--- Darth Vader/Program.cs
+++ Darth Vader/Program.cs
@@ -7,6 +7,7 @@ internal class Program
         Console.WriteLine("Hej jag heter Måns");
         Console.WriteLine("saidj");
         Console.WriteLine("Christofer");
+        Console.WriteLine("hej");
     }
 
 
